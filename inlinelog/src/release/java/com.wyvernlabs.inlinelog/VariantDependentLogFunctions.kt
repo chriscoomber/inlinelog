a5ee8791a20fc37916f3cf5b95eb23313cc20021
chriscoomber <chrisper00@gmail.com>
2021-07-09 15:26:15 +0100

    Suppress unused parameter

--- inlinelog/src/release/java/com.wyvernlabs.inlinelog/VariantDependentLogFunctions.kt
+++ inlinelog/src/release/java/com.wyvernlabs.inlinelog/VariantDependentLogFunctions.kt
@@ -1,5 +1,6 @@
 package com.wyvernlabs.inlinelog
 
+@Suppress("UNUSED_PARAMETER")
 inline fun log(severity: Severity, tag: String?, message: () -> String, e: () -> Throwable? = { null }) {
     // Intentionally a no-op. Because this is inline, the lambda definitions at the call sites will
     // be compiled out, so the logs will not appear in the compiled bytecode.
